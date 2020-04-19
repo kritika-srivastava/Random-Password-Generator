cf0a03e7b89b84739b21b223904d68718fa3190a
Kritika Srivastava <kritika.s1408@gmail.com>
2020-04-19 08:31:31 +0530

    Further modifications w.r.t push Button

--- GUI.py
+++ GUI.py
@@ -1,15 +1,38 @@
+
+#C:\Users\Kritika\Desktop\VSC\python\Random-Password-Generator\GUI.py
+#Kritika Srivastava
+#April 18, 2020
+#Python code to link and interact with the GUI
+
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
-from MainWindow import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
-class AppWindow(QMainWindow, Ui_MainWindow):
+import random
+import string
+
+qtcreator_file  = "MainWindow.ui" # Enter your .ui file here.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
+
+
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.show()  
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.Press_This_Key.clicked.connect(self.RandomNo)
+    
+    def RandomNo(self):
+        str_len = int(self.input.value())
+        def randomString(stringLength):
+                letters = string.ascii_letters
+                return ''.join(random.choice(letters) for i in range(stringLength))
+        out=randomString(str_len)
+        
+        self.Output_Browser.setText(out)
 
-app = QApplication(sys.argv)
-w = AppWindow()
-w.show()
-sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
